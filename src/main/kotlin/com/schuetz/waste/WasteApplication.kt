b1d14fd4fa3ebaaa1960b42d302bca6c517e1a4c
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-06 22:22:15 +0200

    Enable CORS for localhost

--- src/main/kotlin/com/schuetz/waste/WasteApplication.kt
+++ src/main/kotlin/com/schuetz/waste/WasteApplication.kt
@@ -6,6 +6,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class WasteApplication : CommandLineRunner {
@@ -29,4 +33,19 @@ class WasteApplication : CommandLineRunner {
 
 fun main(args: Array<String>) {
     runApplication<WasteApplication>(*args)
+}
+
+/**
+ * Enables CORS for localhost
+ */
+@Configuration
+class MyConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+            }
+        }
+    }
 }
