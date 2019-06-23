3fbe70fbc82c6f9b8b4fbce498dc8c2762739853
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-23 21:42:43 +0200

    Disable spring security dependency and ssl configuration
    Having spring security dependency causes CORS problems.

--- src/main/kotlin/com/schuetz/waste/WasteApplication.kt
+++ src/main/kotlin/com/schuetz/waste/WasteApplication.kt
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -37,13 +33,13 @@ class MyConfiguration {
 }
 
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity) {
-        http.cors().configurationSource {
-            CorsConfiguration().applyPermitDefaultValues()
-        }
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+//
+//    override fun configure(http: HttpSecurity) {
+//        http.cors().configurationSource {
+//            CorsConfiguration().applyPermitDefaultValues()
+//        }
+//    }
+//}
