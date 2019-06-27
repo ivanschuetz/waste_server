cb8ec330e374cf655203745fcf504b0eb39327fd
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-27 21:55:00 +0200

    Remove unnecessary update lines

--- src/main/resources/categories_to_pickup_company.sql
+++ src/main/resources/categories_to_pickup_company.sql
@@ -1,6 +1,6 @@
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(5, 0, null, 'https://www.kraftzone.de/elektroschrott-entsorgung-berlin') ON CONFLICT DO NOTHING;
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 1, null, null) ON CONFLICT DO NOTHING;
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 2, null, 'https://www.bsr.de/sperrmuell-21705.php') ON CONFLICT DO NOTHING;
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 3, null, 'https://www.preiswert-sperrmuell-berlin.de/angebot-anfordern/') ON CONFLICT DO NOTHING;
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 4, null, null) ON CONFLICT DO NOTHING;
-INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 5, null, 'https://www.studenten-power.de/leistungen/blitzangebot-ueber-whatsapp-einholen/') ON CONFLICT DO NOTHING;
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(5, 0, null, 'https://www.kraftzone.de/elektroschrott-entsorgung-berlin');
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 1, null, null);
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 2, null, 'https://www.bsr.de/sperrmuell-21705.php');
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 3, null, 'https://www.preiswert-sperrmuell-berlin.de/angebot-anfordern/');
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 4, null, null);
+INSERT INTO category_pickup_company(category_id, company_id, min_weight, website) VALUES(1, 5, null, 'https://www.studenten-power.de/leistungen/blitzangebot-ueber-whatsapp-einholen/');
