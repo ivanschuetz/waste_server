a8e6955970bdd4e414b24e3e985c6b86db732820
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-27 21:54:27 +0200

    Fix crash with nullable fields

--- src/main/kotlin/com/schuetz/waste/PickupCompanyDTO.kt
+++ src/main/kotlin/com/schuetz/waste/PickupCompanyDTO.kt
@@ -1,4 +1,4 @@
 package com.schuetz.waste
 
-data class PickupCompanyDTO(val id: Long, val name: String, val city: String, val website: String, val phone: String,
-                            val email: String)
+data class PickupCompanyDTO(val id: Long, val name: String, val city: String, val website: String?, val phone: String?,
+                            val email: String?)
