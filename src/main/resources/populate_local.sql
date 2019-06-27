33b86e666c106776c022975e2c250eef0ae77553
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-27 20:10:39 +0200

    Fix file paths

--- src/main/resources/populate_local.sql
+++ src/main/resources/populate_local.sql
@@ -1,4 +1,4 @@
---\i /Users/ischuetz/dev/waste/server/src/main/resources/populate.sql
+--\i /Users/ischuetz/dev/waste/server/src/main/resources/populate_local.sql
 
 BEGIN;
 \i /Users/ischuetz/dev/waste/server/src/main/resources/init.sql
@@ -7,14 +7,14 @@ BEGIN;
 \i /Users/ischuetz/dev/waste/server/src/main/resources/containers.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/p_containers.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/categories.sql
+\i /Users/ischuetz/dev/waste/waste_scrap/src/main/resources/p_containers_bsr.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/categories_to_containers.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/pickup_companies.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/categories_to_pickup_company.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/categories_to_p_containers.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/items.sql
-\i /Users/ischuetz/dev/waste/waste_json_parser/src/main/resources/output.sql
-\i /Users/ischuetz/dev/waste/waste_scrap/src/main/resources/output.sql
-\i /Users/ischuetz/dev/waste/altkleider/src/main/resources/output.sql
-\i /Users/ischuetz/dev/waste/altkleider/src/main/resources/output_item.sql
+\i /Users/ischuetz/dev/waste/waste_json_parser/src/main/resources/p_containers_glasiglas.sql
+\i /Users/ischuetz/dev/waste/altkleider/src/main/resources/p_containers_clothes.sql
+\i /Users/ischuetz/dev/waste/altkleider/src/main/resources/p_containers_clothes_item.sql
 \i /Users/ischuetz/dev/waste/server/src/main/resources/opening_hours.sql
 COMMIT;
