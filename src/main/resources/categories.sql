db831e0ba67fbd4c4bd7818a5d6628188b3c8d7c
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-29 15:41:02 +0200

    Add categories and notes

--- src/main/resources/categories.sql
+++ src/main/resources/categories.sql
@@ -4,7 +4,13 @@ INSERT INTO category(id, name) VALUES(2, 'category_paper') ON CONFLICT (name) DO
 INSERT INTO category(id, name) VALUES(3, 'category_energy_saving_lamps') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO category(id, name) VALUES(4, 'category_glas_white') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO category(id, name) VALUES(5, 'category_electro') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
-INSERT INTO category(id, name) VALUES(6, 'category_household') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
+-- TODO hausmuell in bsr is not residual! it means paper|residual|bio|glas|electro
+-- TODO usw. siehe https://de.wikipedia.org/wiki/Hausm%C3%BCll
+-- TODO difference is the quantity -- do we need a new construct for this or are new categories enough?
+-- TODO (new categories for large quantities, where "large" appears in translation. Default is non-large.)
+-- TODO this means that we have to associate BSR both with the "hausmuell" cats + new large cats, where applicable
+-- TODO don't forget to preserve current category ids in BSR output file, as these are references from sql files here!
+INSERT INTO category(id, name) VALUES(6, 'category_residual') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO category(id, name) VALUES(7, 'category_bio') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO category(id, name) VALUES(8, 'category_glas_green') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 INSERT INTO category(id, name) VALUES(9, 'category_clothes') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
@@ -18,5 +24,7 @@ INSERT INTO category(id, name) VALUES(12, 'category_dead_pet') ON CONFLICT (name
 -- new
 --
 INSERT INTO category(id, name) VALUES(13, 'category_car_parts') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
+INSERT INTO category(id, name) VALUES(14, 'category_drain') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
+INSERT INTO category(id, name) VALUES(15, 'category_toilet') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name;
 -- TODO p containers/ pickup
 
