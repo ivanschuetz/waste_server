b2ddc0b808f31b0aa5b66cfb836fe1670766ae96
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-30 14:31:24 +0200

    Darker green

--- src/main/resources/containers.sql
+++ src/main/resources/containers.sql
@@ -5,7 +5,7 @@ INSERT INTO container(id, name, color) VALUES(2, 'container_bio', 'CC7722') ON C
 INSERT INTO container(id, name, color) VALUES(5, 'container_bulky', '666666') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
 INSERT INTO container(id, name, color) VALUES(6, 'container_blue', '0000FF') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
 INSERT INTO container(id, name, color) VALUES(7, 'container_glas_white', 'FFFFFF') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
-INSERT INTO container(id, name, color) VALUES(8, 'container_glas_green', '00FF00') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
+INSERT INTO container(id, name, color) VALUES(8, 'container_glas_green', '006400') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
 INSERT INTO container(id, name, color) VALUES(9, 'container_glas_brown', 'A52A2A') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
 INSERT INTO container(id, name, color) VALUES(10, 'container_scrap', 'FFA500,FFFF00') ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, color = EXCLUDED.color;
 
